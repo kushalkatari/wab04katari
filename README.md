86e92faf796019a99cfaf82950edf5d8406df876
kushalkatari <35985697+kushalkatari@users.noreply.github.com>
2021-03-31 20:52:07 -0500

    final commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# wab04katari
+Heroku Link: https://dashboard.heroku.com/apps/wab04katari</br>
+my class Dresses attribute name (string),cost(number),brand (number)
--- app.js
+++ app.js
@@ -6,8 +6,11 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
+var DressesRouter = require('./routes/Dresses');
+var starsRouter = require('./routes/stars');
+var slotsRouter = require('./routes/slots');
 var app = express();
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,6 +24,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/Dresses', DressesRouter);
+app.use('/stars', starsRouter);
+app.use('/slots', slotsRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
